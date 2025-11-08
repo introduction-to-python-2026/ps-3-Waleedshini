c1b2ed059c5e42004537044c66428311592c4e88
Waleedshini <walid.shini@live.biu.ac.il>
2025-11-08 18:41:05 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,2 +1,13 @@
+from fractions import Fraction
 def approximate_pi(n_terms):
-    pass # replace pass with your code
+    list_of_numbers = []
+    leibniz_series = []
+    for i in range(n_terms):
+        list_of_numbers.append(i)
+        if i % 2 == 0:
+            list_of_numbers[i] = 1
+        else:
+          list_of_numbers[i] = -1
+        leibniz_series.append(Fraction(list_of_numbers[i], (2*i + 1))) 
+    pi_approximate = 4* sum(leibniz_series)
+    return (pi_approximate)
